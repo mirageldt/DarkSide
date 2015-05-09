dc5f65f360739aaebb0c1b56aa2b92fd98bcc4cf
deoncoke <deoncoke@outlook.com>
2015-05-09 10:46:19 -0700

    Added different project

---
+++ Project/Wins/VisualStudio/DSMOON/DSMOON/DSMOON.cpp
@@ -0,0 +1,18 @@
+// DSMOON.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+//#include
+
+int main(int argc, _TCHAR* argv[])
+{
+	char c;
+	printf("hello world");
+
+	c = _fgetchar();
+	getchar();
+
+	return 0;
+}
+
