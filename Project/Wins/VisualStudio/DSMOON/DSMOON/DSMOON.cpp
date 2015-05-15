54696d833ef9adb10bb174f8daa79e5b2828e865
deoncoke <deoncoke@outlook.com>
2015-05-15 01:06:06 -0500

    set to go

--- Project/Wins/VisualStudio/DSMOON/DSMOON/DSMOON.cpp
+++ Project/Wins/VisualStudio/DSMOON/DSMOON/DSMOON.cpp
@@ -4,14 +4,31 @@
 #include <SDL2\SDL.h>
 #undef main
 //#include
-SDL_Window * mWindow;
-SDL_Renderer * mRender;
+
+bool init(){ return true;
+
+
+} 
+
+
 int main()
 {
-	SDL_Init(SDL_INIT_VIDEO);
-	mWindow =SDL_CreateWindow(
-	
+	SDL_Window * mWindow;
+SDL_Renderer *mRender = 0;
 
+	SDL_Init(SDL_INIT_VIDEO);
+	mWindow =SDL_CreateWindow("Darkside",SDL_WINDOWPOS_CENTERED, 
+		SDL_WINDOWPOS_CENTERED,800,600,SDL_WINDOW_BORDERLESS); 
+	if (mWindow !=0){
+		
+		mRender =SDL_CreateRenderer(mWindow,-1,SDL_RENDERER_ACCELERATED);
+		
+		if (!mRender){SDL_Quit();}
+	
+	}
+	SDL_RenderPresent(mRender);
+	SDL_Delay(10000);
+	SDL_Quit();
 	return 0;
 }
 
